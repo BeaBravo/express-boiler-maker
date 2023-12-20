2a1f1289b287f9772c97555c17242c96950ac2c4
Beatriz Bravo <beatz.bravo@gmail.com>
2023-12-20 13:18:25 -0700

    added boiler plate info server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 // express
 const express = require("express");
+const path = require("path");
 
 // DATA
 
@@ -15,6 +16,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // ROUTES
+// html routes - deliver pages
+// GET / - the home page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+
+//api routes
+app.get("api/todos", (req, res) => res.json([{ title: "buy milk" }]));
+
+//404 route
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/404.html"))
+);
 
 // START SERVER
 app.listen(PORT, () => console.log(`Server live on ${PORT}`));
